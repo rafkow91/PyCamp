2f2f68e73b6776f8874248a96a9c6de0096d3343
Rafał Kowalski <rafkow91@gmail.com>
2022-03-06 01:09:55 +0100

    Next stage of coding OOP apps

--- Module_00/totolotek_oop.py
+++ Module_00/totolotek_oop.py
@@ -1,6 +1,8 @@
 """OOP application of Lotto Game"""
 from random import randint
 
+TICKET_COST = 3.00
+
 THIRD_DEGREE_REWARD = 24.00
 FOURTH_DEGREE_REWARD = 245.10
 FIFTH_DEGREE_REWARD = 7706.80
@@ -72,7 +74,6 @@ class Game:
             scores (list): List of winnings degree. Index of list is a winning degree.
         """
         self.player = player
-        self.numbers = player.numbers
         self.scores = [0, 0, 0, 0, 0, 0, 0]
 
     @staticmethod
@@ -89,36 +90,92 @@ class Game:
 
         return result
 
-    def winning_degree(self) -> int:
+    def check_winning_degree(self) -> int:
         """_summary_"""
         drawing_numbers = self._draw()
         
-        return len(self.numbers.intersection(drawing_numbers))
+        return len(self.player.numbers.intersection(drawing_numbers))
 
-    def add_to_scores_list(self, winning_degree: int = 0):
+    def add_to_scores_list(self, winning_degree: int = 0) -> list:
         self.scores[winning_degree] += 1
 
-    def calculate_rewards(self):
+
+    def play_while(self, winning_degree: int = 6) -> int:
+        plays = 0
+        while self.scores[winning_degree] < 1:
+            self.add_to_scores_list(self.check_winning_degree())
+            plays += 1
+
+        return plays
+        
+    def calculate_rewards(self) -> float:
         summary = 0
         for index, score in enumerate(self.scores):
             summary += score * REWARDS[index]
 
-        return summary
+        return round(summary, 2)
 
+    def calculate_costs(self) -> int:
+        cost = 0
+        for score in self.scores:
+            cost += score * TICKET_COST 
+
+        return round(cost, 2)
+        
+    def calculate_profit(self) -> int:
+        return self.calculate_rewards() - self.calculate_costs()
+
+    def calculate_age(self) -> int:
+        years = 0
+        for score in self.scores:
+            years += score / WEEKS_OF_YEAR
+
+        return self.player.age + years
+
+# class Application:
+#     def __init__(self) -> None:
+#         self.LABEL = 'Main menu'
+#         self.MENU = {
+#             1: self.show_user_data,
+#         #     2: self.input_user_data,
+#         #     3: self.input_numbers,
+#         #     4: self.play_x_times,
+#         #     5: self.play_while,
+#         #     0: self.quit
+#         }
+#         self.user = User()
+
+#     def __str__(self) -> str:
+#         return self.LABEL
+
+#     def draw(self):
+#         print(self.LABEL)
+#         for key, value in self.MENU.items():
+#             print(f'{key} -> {value}')
+#         return int(input('What do you want to do?  '))
+        
+
+#     def show_user_data(self):
+#         class Meta:
+#             self.LABEL = 'Show User data'
+        
+    
+        self.draw()
+        
+        print(self.user)
 
 if __name__ == '__main__':
-    raf = User()
-    raf.input_numbers({13, 9, 29, 6, 24, 4})
-
+    raf = User('Rafal', 31, {13, 9, 29, 6, 24, 4})
+    
     gra = Game(raf)
-    plays = 0
-
-    while gra.scores[6] < 1:
-        gra.add_to_scores_list(gra.winning_degree())
-        plays += 1
-
+    # app = Application()
+    # app.draw()
+    degree = int(input('Winning degree to calculate: '))
     print(gra.player)
-    print(f'{plays:_}')
-    print(f'{gra.calculate_rewards():_}')
-    print(f'{gra.calculate_rewards()-3*plays:_}')
+    print(f'Plays: {gra.play_while(degree):_}')
+    print(f'Rewards sum: {gra.calculate_rewards():_} zł')
+    print(f'Tickets costs: {gra.calculate_costs():_} zł')
+    print(f'Bilans: {gra.calculate_profit():_} zł')
+    print(f'Age when you win {degree}: {gra.calculate_age():_} y/o')
+
 

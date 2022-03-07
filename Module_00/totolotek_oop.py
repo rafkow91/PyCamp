76f5261e45eb9bd3118fb6c86cbed61fa84606fc
Rafał Kowalski <rafkow91@gmail.com>
2022-03-07 22:45:20 +0100

    Final version of totolotek_oop.py without docstrings

--- Module_00/totolotek_oop.py
+++ Module_00/totolotek_oop.py
@@ -1,5 +1,8 @@
 """OOP application of Lotto Game"""
 from random import randint
+from os import system
+from time import sleep
+import sys
 
 TICKET_COST = 3.00
 
@@ -8,15 +11,19 @@ FOURTH_DEGREE_REWARD = 245.10
 FIFTH_DEGREE_REWARD = 7706.80
 SIX_DEGREE_REWARD = 6000000.00
 
-REWARDS = [0, 0, 0, THIRD_DEGREE_REWARD, FOURTH_DEGREE_REWARD, FIFTH_DEGREE_REWARD, SIX_DEGREE_REWARD]
+REWARDS = [0, 0, 0, THIRD_DEGREE_REWARD, FOURTH_DEGREE_REWARD,
+           FIFTH_DEGREE_REWARD, SIX_DEGREE_REWARD]
 
 WEEKS_OF_YEAR = 52.177457
 
+
 class User:
-    def __init__(self, name: str = None, age: int = None, numbers: set = {1, 2, 3, 4, 5, 6}) -> None:
+    def __init__(self, name: str = None, age: int = None, numbers: set = None) -> None:
         self.name = name
         self.age = age
         self.numbers = numbers
+        if self.numbers is None:
+            self.numbers = {1, 2, 3, 4, 5, 6}
 
     def input_personal_data(self):
         if self.name is None:
@@ -31,15 +38,13 @@ class User:
                     choice = input('Do you try again? [y/n]  ')
                     if choice == '' or choice[0].lower() == 'y':
                         continue
-                    else:
-                        break
 
                 break
 
     def input_numbers(self, numbers: set = None):
         if numbers is not None:
             self.numbers = numbers
-        
+
         else:
             new_numbers = set()
 
@@ -93,12 +98,11 @@ class Game:
     def check_winning_degree(self) -> int:
         """_summary_"""
         drawing_numbers = self._draw()
-        
+
         return len(self.player.numbers.intersection(drawing_numbers))
 
     def add_to_scores_list(self, winning_degree: int = 0) -> list:
         self.scores[winning_degree] += 1
-
 
     def play_while(self, winning_degree: int = 6) -> int:
         plays = 0
@@ -107,7 +111,7 @@ class Game:
             plays += 1
 
         return plays
-        
+
     def calculate_rewards(self) -> float:
         summary = 0
         for index, score in enumerate(self.scores):
@@ -118,64 +122,146 @@ class Game:
     def calculate_costs(self) -> int:
         cost = 0
         for score in self.scores:
-            cost += score * TICKET_COST 
+            cost += score * TICKET_COST
 
         return round(cost, 2)
-        
+
     def calculate_profit(self) -> int:
-        return self.calculate_rewards() - self.calculate_costs()
+        return round(self.calculate_rewards() - self.calculate_costs(), 2)
 
     def calculate_age(self) -> int:
         years = 0
         for score in self.scores:
             years += score / WEEKS_OF_YEAR
 
-        return self.player.age + years
+        return round(self.player.age + years, 2)
 
-# class Application:
-#     def __init__(self) -> None:
-#         self.LABEL = 'Main menu'
-#         self.MENU = {
-#             1: self.show_user_data,
-#         #     2: self.input_user_data,
-#         #     3: self.input_numbers,
-#         #     4: self.play_x_times,
-#         #     5: self.play_while,
-#         #     0: self.quit
-#         }
-#         self.user = User()
+    def print_summary(self):
+        print('\n\n' + '-'*10 + '   S U M M A R Y   ' + '-'*10)
+        print(f'Plays:\t{sum(self.scores)}')
+        print(f'Tickets cost:\t{self.calculate_costs()} PLN')
+        print(f'Sum of rewards:\t{self.calculate_rewards()} PLN')
+        print(f'Bilans:\t{self.calculate_profit()} PLN\n')
 
-#     def __str__(self) -> str:
-#         return self.LABEL
+        if self.calculate_profit() < 0:
+            print('Oh no! You are bankrupt!!')
+        else:
+            print('Congratulation :)')
 
-#     def draw(self):
-#         print(self.LABEL)
-#         for key, value in self.MENU.items():
-#             print(f'{key} -> {value}')
-#         return int(input('What do you want to do?  '))
-        
 
-#     def show_user_data(self):
-#         class Meta:
-#             self.LABEL = 'Show User data'
-        
-    
-        self.draw()
-        
-        print(self.user)
+class Application:
+    MENU = {
+        1: 'Show user data',
+        2: 'Input user data',
+        3: 'Input numbers',
+        4: 'Play "x" times',
+        5: 'Play while you win x',
+        0: 'Exit',
+    }
+
+    def __init__(self) -> None:
+        self.user = User()
+        self.choice = None
+        self.label = None
+
+    def __str__(self) -> str:
+        return self.label
+
+    def _draw_logo(self):
+        system('clear')
+        weight = len(self.label) + 35
+        print('', '-' * weight, '\n |', ' ' * 15, self.label, ' ' * 14, '|\n',
+              '-' * weight, '\n\n')
+
+    def _draw_main_menu(self):
+        self.label = 'Main menu'
+        self._draw_logo()
+        for key, value in self.MENU.items():
+            print(f'{key} -> {value}')
+        print()
+
+    def _get_choice(self) -> int:
+        choice = None
+
+        while choice not in self.MENU.items():
+            try:
+                choice = int(input('Choose option: '))
+                if choice not in self.MENU.items():
+                    raise ValueError
+            except ValueError:
+                print('You input a wrong number')
+                continue
+
+        self.choice = choice
+
+    def _go_to_choiced_option(self):
+        if self.choice == 0:
+            if self.user.name is not None:
+                print(f'See you later {self.user.name} ;)')
+            else:
+                print('Goodbye')
+            sleep(1)
+            sys.exit()
+        elif self.choice == 1:
+            print(self.user)
+        elif self.choice == 2:
+            self.user.input_personal_data()
+        elif self.choice == 3:
+            self.user.input_numbers()
+        elif self.choice == 4:
+            self._play_x_times()
+        elif self.choice == 5:
+            self._play_while()
+
+    def _play_x_times(self):
+        game = Game(self.user)
+
+        while True:
+            try:
+                amount_of_repetitions = int(input('How many times to draw? '))
+                if amount_of_repetitions < 1:
+                    raise ValueError
+            except ValueError:
+                print('You must input a positive integer. Try again!')
+                continue
+
+            break
+
+        for _ in range(amount_of_repetitions):
+            game.add_to_scores_list(game.check_winning_degree())
+
+        game.print_summary()
+
+    def _play_while(self):
+        game = Game(self.user)
+
+        while True:
+            try:
+                winning_degree = int(input('How many degree you want to win? '))
+                if winning_degree < 0 or winning_degree > 7:
+                    raise ValueError
+            except ValueError:
+                print('You must input a positive integer >0 and <7. Try again!')
+                continue
+
+            break
+
+        game.play_while(winning_degree)
+        game.print_summary()
+
+        if self.user.age is not None:
+            print(f'When you win you will be {game.calculate_age()} years old.')
+
+    def run(self):
+        self._draw_main_menu()
+        self._get_choice()
+        self.label = self.MENU[self.choice]
+        self._draw_logo()
+        self._go_to_choiced_option()
+        input('\n\nPress any key to continue')
+        self.run()
+
 
 if __name__ == '__main__':
-    raf = User('Rafal', 31, {13, 9, 29, 6, 24, 4})
-    
-    gra = Game(raf)
-    # app = Application()
-    # app.draw()
-    degree = int(input('Winning degree to calculate: '))
-    print(gra.player)
-    print(f'Plays: {gra.play_while(degree):_}')
-    print(f'Rewards sum: {gra.calculate_rewards():_} zł')
-    print(f'Tickets costs: {gra.calculate_costs():_} zł')
-    print(f'Bilans: {gra.calculate_profit():_} zł')
-    print(f'Age when you win {degree}: {gra.calculate_age():_} y/o')
-
-
+    app = Application()
+    app.run()
